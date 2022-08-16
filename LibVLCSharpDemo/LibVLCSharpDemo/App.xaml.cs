a20f36fa8023ab44adcb5364208fbd1f6ec32d39
sukhesh.majalkar <sukhesh.majalkar@xtreme-media.in>
2022-08-16 19:14:02 +0530

    demo

---
+++ LibVLCSharpDemo/LibVLCSharpDemo/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LibVLCSharpDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Core.Initialize(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "LibVlc", "win-x86"));
+        }
+       
+    }
+}
---
+++ LibVLCSharpDemo/LibVLCSharpDemo/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace LibVLCSharpDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        LibVLC _libVLC;
+        List<string> VideoList = new List<string>();
+        int currentIndex = 0;
+        DispatcherTimer Timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            VideoList.Add(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Videos", "1.mp4"));
+            VideoList.Add(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Videos", "2.mp4"));
+            Timer.Interval = TimeSpan.FromSeconds(20);
+            Timer.Tick += Timer_Tick;
+            _libVLC = new LibVLC(enableDebugLogs: true);
+            VideoView.MediaPlayer = new LibVLCSharp.Shared.MediaPlayer(_libVLC);
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (VideoView.MediaPlayer.IsPlaying)
+                {
+                    VideoView.MediaPlayer.Pause();
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            PlayNext();
+        }
+        private void PlayNext()
+        {
+            try
+            {
+                Timer.Stop();
+
+                Play(VideoList[currentIndex]);
+                currentIndex++;
+                if (currentIndex >= VideoList.Count)
+                {
+                    currentIndex = 0;
+                }
+                Timer.Start();
+
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        private void Play(string VideoPath)
+        {
+            try
+            {
+                using (var media = new Media(_libVLC, new Uri(VideoPath)))
+                {
+                    media.AddOption(new MediaConfiguration { EnableHardwareDecoding = true});
+                    VideoView.MediaPlayer.Play(media);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PlayNext();
+        }
+    }
+}
